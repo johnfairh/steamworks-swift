f3bf387c59c8fef21d9275724ebdf9a01f948ec1
John Fairhurst <johnfairh@gmail.com>
2022-07-17 12:54:00 +0100

    Add GetImageRGBA helper

--- Sources/SteamworksHelpers/SteamUtils+Helpers.swift
+++ Sources/SteamworksHelpers/SteamUtils+Helpers.swift
@@ -12,4 +12,13 @@ extension SteamUtils {
     public func filterText(context: TextFilteringContext, steamID: SteamID, inputMessage: String) -> (rc: Int, filteredText: String) {
         filterText(context: context, steamID: steamID, inputMessage: inputMessage, byteSizeOutFilteredText: inputMessage.utf8.count + 1)
     }
+
+    /// Steamworks `ISteamUtils::GetImageRGBA()`
+    public func getImageRGBA(imageIndex: Int) -> (rc: Bool, dest: [UInt8]) {
+        let (rc, width, height) = getImageSize(imageIndex: imageIndex)
+        guard rc else {
+            return (rc, [])
+        }
+        return getImageRGBA(imageIndex: imageIndex, destBufferSize: width * height * 4) // per docs
+    }
 }
